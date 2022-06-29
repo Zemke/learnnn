138126ffbb4ebab1db445ca9d6f6aca047a55390
Zemke <florian@zemke.io>
2022-06-29 09:46:14 +0200

    Never needed DD

--- data.py
+++ data.py
@@ -9,9 +9,6 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 
-DD = os.environ.get('DEBUG', '0') == '1'
-
-
 class NNNData(Dataset):
 
   def __init__(self, season, mmn = None):
@@ -56,16 +53,13 @@ class NNNData(Dataset):
           'oppOpps': 0,
         }
         opps = []
-        DD and print('user ' + u)
         for l in ll:
           if l[0] == u:
-            DD and print(f'{l[2]}-{l[3]} {l[1]}')
             pu['won'] += l[2]
             pu['lost'] += l[3]
             pu['opps'].add(l[1])
             pu['total'] += l[2] + l[3]
           elif l[1] == u:
-            DD and print(f'{l[3]}-{l[2]} {l[0]}')
             pu['won'] += l[3]
             pu['lost'] += l[2]
             pu['opps'].add(l[0])
@@ -73,13 +67,10 @@ class NNNData(Dataset):
         self.puu.append(pu)
 
       for pu in self.puu:
-        DD and print('user opping ' + pu['u'])
-        DD and print(len(pu['opps']))
         for uopp in pu['opps']:
           for opp in self.puu:
             if opp['u'] != uopp:
               continue
-            DD and print('opp ' + opp['u'])
             pu['oppWon'] += opp['won']
             pu['oppLost'] += opp['lost']
             pu['oppTotal'] += opp['total']
@@ -87,9 +78,6 @@ class NNNData(Dataset):
 
       self.total = sum([pu['total'] for pu in self.puu]) // 2
       self.total_users = len(self.puu)
-
-      DD and print('total', self.total)
-      DD and print('total_users', self.total_users)
 
 
   def __len__(self):
@@ -99,7 +87,6 @@ class NNNData(Dataset):
   def __getitem__(self, idx):
     u,p = self.labels.iloc[idx, 1:3]
     pu = [pu for pu in self.puu if pu['u'] == u][0]
-    DD and print(pu['u'], p)
     if self.mmn is not None and self.mmn.ready:
       return \
         self.mmn.norm_x(self.to_input(pu)), \
